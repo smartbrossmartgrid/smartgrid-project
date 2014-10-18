b3ecf5058b0dc36fa22134cf1b922082149ce9ac
Raúl Suárez <raul.suarez.marin@gmail.com>
2014-10-18 12:55:56 +0200

    tools para google maps api.
    Quiza no funcionan porque necesitan autenticarse

---
+++ src/thesmartbros/sagilbe/tools/JSONParser.java
@@ -0,0 +1,57 @@
+package thesmartbros.sagilbe.tools;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+/**
+ * Created by McD0n3ld on 20/09/2014.
+ */
+public class JSONParser {
+    InputStream is = null;
+    JSONObject jObj = null;
+    String json = "";
+
+    public String getJSONFromUrl(String url) {
+        // Making HTTP request
+        try {
+            // defaultHttpClient
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            is = httpEntity.getContent();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+
+            json = sb.toString();
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+}
--- src/thesmartbros/sagilbe/tools/ToolsMap.java
+++ src/thesmartbros/sagilbe/tools/ToolsMap.java
@@ -1,5 +1,50 @@
 package thesmartbros.sagilbe.tools;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class ToolsMap {
 
+	public static String getLocationFromName(double latitude, double longitude) {
+		String URL = makeURLforRouteName(latitude, longitude);
+		String result = (new JSONParser()).getJSONFromUrl(URL);
+		String[] results = retrieveNameGeo(result);
+		if (results == null)
+			return latitude+","+longitude;
+		else
+			return results[0]+results[1];
+    }
+	
+	private static String makeURLforRouteName(double latitude, double longitude) {
+        StringBuilder urlString = new StringBuilder();
+        urlString.append("http://maps.googleapis.com/maps/api/geocode/json");
+        urlString.append("?latlng=");// from
+        urlString.append(latitude);
+        urlString.append(",");
+        urlString.append(longitude);
+        urlString.append("&sensor=false");
+        return urlString.toString();
+    }
+
+	private static String[] retrieveNameGeo(String result) {
+        try {
+            JSONArray resultsArray = (new JSONObject(result)).getJSONArray("results");
+            JSONObject results = resultsArray.getJSONObject(0);
+            JSONArray myResults = results.getJSONArray("address_components");
+            String route = myResults.getJSONObject(1).getString("short_name");
+            String locality = myResults.getJSONObject(2).getString("short_name");
+            if (route == null || locality == null)
+            	return null;
+            else {
+            	String[] res = new String[2];
+            	res[0] = route;
+            	res[1] = locality;            	
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }	
 }
