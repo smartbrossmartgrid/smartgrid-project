81a39633c93e8e64d256380856c04cddd6eb49bc
Joan93 <jfloritmir@gmail.com>
2014-10-19 15:36:39 +0200

    Initial Commit

--- src/thesmartbros/sagilbe/classes/proveedor/Proveedor.java
+++ src/thesmartbros/sagilbe/classes/proveedor/Proveedor.java
@@ -1,5 +1,28 @@
 package thesmartbros.sagilbe.classes.proveedor;
 
+import java.util.List;
+
+//clase perteneciente al objeto proveedor
+
 public class Proveedor {
+
+	// lista de zonas
+	private List<Zona> miszonas;
+
+	private int consumoTotal;
+
+	
+	//cálculo del precio
+	private void calcular_precio(int consumo) {
+
+	}
+
+	
+	//para enviar al técnico
+	private void enviar_tecnico() {
+
+	}
+	
+	
 
 }
