74b5d0a9cb80a0a5dafc2fe20241222f3f3a4778
Joan93 <jfloritmir@gmail.com>
2014-10-19 15:40:11 +0200

    empezando con zona y proveedor

--- src/thesmartbros/sagilbe/classes/proveedor/Zona.java
+++ src/thesmartbros/sagilbe/classes/proveedor/Zona.java
@@ -1,5 +1,24 @@
 package thesmartbros.sagilbe.classes.proveedor;
 
+
+//clase de la zona que se analiza
 public class Zona {
 
+	//id de la zona
+	private int ID;
+	
+	//viviendas que tiene, su gasto...
+	private int numero_viviendas;
+	private int gasto_energetico;
+	private float porcentaje_gasto;
+	
+	//tecnicos, ocupados y disponibles
+	
+	private int tecnicos_disp;
+	private int tecnicos_ocup;
+	
+	//averias solucionadas
+	private int averias_sol;
+	
+	
 }
