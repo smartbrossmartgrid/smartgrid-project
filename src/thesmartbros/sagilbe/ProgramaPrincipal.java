6947f533eb5c862234671db299974ca6adb51ecf
Murillo <eric.murillo93@gmail.com>
2014-10-20 11:21:46 +0200

    Prueba Eric

--- src/thesmartbros/sagilbe/ProgramaPrincipal.java
+++ src/thesmartbros/sagilbe/ProgramaPrincipal.java
@@ -3,6 +3,6 @@ package thesmartbros.sagilbe;
 public class ProgramaPrincipal {
 	
 	public static void main(String args[]) {
-		System.out.println("works");
+		System.out.println("Prueba Eric");
 	}
 }
