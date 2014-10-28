6a658fc1d569dd13b20d80f593354a34e07603d3
arnau.garcia.casals <arnau193@gmail.com>
2014-10-28 12:17:08 +0100

    Parte casa

--- src/thesmartbros/sagilbe/classes/casa/Contador.java
+++ src/thesmartbros/sagilbe/classes/casa/Contador.java
@@ -2,4 +2,50 @@ package thesmartbros.sagilbe.classes.casa;
 
 public class Contador {
 
+	private float precio_acumulado;
+	private float precio_actual;
+	private int energia_consumida;
+	private float latitud;
+	private float longitud;
+
+	public float getPrecio_acumulado() {
+		return precio_acumulado;
+	}
+
+	public void setPrecio_acumulado(float precio_acumulado) {
+		this.precio_acumulado = precio_acumulado;
+	}
+
+	public float getPrecio_actual() {
+		return precio_actual;
+	}
+
+	public void setPrecio_actual(float precio_actual) {
+		this.precio_actual = precio_actual;
+	}
+
+	public int getEnergia_consumida() {
+		return energia_consumida;
+	}
+
+	public void setEnergia_consumida(int energia_consumida) {
+		this.energia_consumida = energia_consumida;
+	}
+
+	public float getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(float latitud) {
+		this.latitud = latitud;
+	}
+
+	public float getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(float longitud) {
+		this.longitud = longitud;
+	}
+
 }
--- src/thesmartbros/sagilbe/classes/casa/Electrodomestico.java
+++ src/thesmartbros/sagilbe/classes/casa/Electrodomestico.java
@@ -2,4 +2,50 @@ package thesmartbros.sagilbe.classes.casa;
 
 public class Electrodomestico {
 
+	private long tiempo_uso;
+	private long tiempo_apagado;
+	private String nombre;
+	private int coste_energetico;
+	private boolean estado;
+
+	public long getTiempo_uso() {
+		return tiempo_uso;
+	}
+
+	public void setTiempo_uso(long tiempo_uso) {
+		this.tiempo_uso = tiempo_uso;
+	}
+
+	public long getTiempo_apagado() {
+		return tiempo_apagado;
+	}
+
+	public void setTiempo_apagado(long tiempo_apagado) {
+		this.tiempo_apagado = tiempo_apagado;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCoste_energetico() {
+		return coste_energetico;
+	}
+
+	public void setCoste_energetico(int coste_energetico) {
+		this.coste_energetico = coste_energetico;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
 }
