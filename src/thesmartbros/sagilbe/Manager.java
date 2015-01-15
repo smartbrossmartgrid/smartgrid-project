baa71e62de519eea7d84d5514098d6666dbf1529
McD0n3ld <McD0n3ld@192.168.1.128>
2015-01-15 19:42:03 +0100

    jsonarray

--- src/thesmartbros/sagilbe/Manager.java
+++ src/thesmartbros/sagilbe/Manager.java
@@ -1,17 +1,16 @@
 package thesmartbros.sagilbe;
 
-import java.awt.Container;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import thesmartbros.sagilbe.classes.proveedor.Zona;
-import thesmartbros.sagilbe.classes.proveedor.Proveedor.Tiempo;
-import thesmartbros.sagilbe.tools.Paillier;
+import thesmartbros.sagilbe.classes.casa.ElectrodomesticoJSON;
 import thesmartbros.sagilbe.tools.PrinterTools;
 import thesmartbros.sagilbe.tools.SocketTools;
 import thesmartbros.sagilbe.tools.ToolsMap;
@@ -21,7 +20,7 @@ public class Manager {
 
 	/* variables principales, de socket */
 	private ServerSocket serverSocket = null;
-	
+
 	public void start() {
 		final boolean listening = true;
 		try {
@@ -46,15 +45,12 @@ public class Manager {
 				PrinterTools.socketLog("Client connected to Manager... OK (" + socket + ")");
 				String jsonMessageFromContador = SocketTools.getJSON(socket); // desde el socket conseguir el JSON
 				Container c = parseJSON(jsonMessageFromContador); //parsear el JSON
-				if (c.type == VariablesGlobales._MESSAGE_TYPE_ALERTA_CONSUMO_SUPERADO) {
-				}
+				if (c.type == VariablesGlobales._MESSAGE_TYPE_ALERTA_CONSUMO_SUPERADO) {}
 			}
 		});
 		t.start();
 	}
-		
-	
-	
+
 	private Container parseJSON(String jsonMessage) {
 		Container c = new Container();
 		float longitud = 0;
@@ -69,8 +65,18 @@ public class Manager {
 				contadorid = jsonObject.getInt("contadorId");
 				longitud = Float.parseFloat(jsonObject.getString("longitud"));
 				latitud = Float.parseFloat(jsonObject.getString("latitud"));
+				JSONArray array = jsonObject.getJSONArray("turnedOnDevices");
+				List<ElectrodomesticoJSON> turnedOnDevices = new ArrayList<ElectrodomesticoJSON>();
+				for (int i = 0; i < array.length(); i++) {
+					JSONObject jsonArrayObject = array.getJSONObject(i);
+					int consumo = jsonArrayObject.getInt("consumo");
+					boolean encendido = jsonArrayObject.getBoolean("encendido");
+					String nombre = jsonArrayObject.getString("nombre");
+					ElectrodomesticoJSON ejson = new ElectrodomesticoJSON(nombre, consumo, encendido);
+					turnedOnDevices.add(ejson);
 				}
-				
+			}
+
 		} catch (NumberFormatException | JSONException e) {
 			e.printStackTrace();
 		}
@@ -80,17 +86,16 @@ public class Manager {
 		c.latitud = latitud;
 		return c;
 	}
-	
-	
+
 	private class Container {
 		public int type = 0;
 		public int contadorid = 0;
 		public float longitud = 0;
 		public float latitud = 0;
 	}
-	
+
 	private String getStreet(float latitud, float longitud) {
 		return ToolsMap.getLocationFromName(latitud, longitud);
 	}
-	
+
 }
